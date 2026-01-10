596096445f581674ae88307de6d1de582fc583fb
patientx <patient.x@gmail.com>
2026-01-10 03:02:52 +0300

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,7 @@
+WEB_DIRECTORY = "./web"
+
+# Frontend-only extension - no Python nodes
+NODE_CLASS_MAPPINGS = {}
+NODE_DISPLAY_NAME_MAPPINGS = {}
+
+__all__ = ["WEB_DIRECTORY", "NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
